52534e8e12f07fcdc1f2633b2171134c2f05c2e9
ianbaremans <32411363+ianbaremans@users.noreply.github.com>
2017-10-06 12:43:37 +0200

    added vars to store data

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import asciimatics
 # getting the webpage with data and parse it to tree
 page = requests.get("https://charts.bitcoin.com/")
 tree = html.fromstring(page.content)
+
+# store data from webpage in vars
+btc_price = tree.xpath("//p[@id='bb-price']/text()")
+
+print(btc_price)
