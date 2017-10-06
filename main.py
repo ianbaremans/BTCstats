6c21fbd07b3398392e4628bb39d5fc4e1a4c3cc0
ianbaremans <32411363+ianbaremans@users.noreply.github.com>
2017-10-06 14:03:11 +0200

    Added variables for USD and EUR

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import requests
 import asciimatics
 
 # getting the webpage with data and parse it to tree
-page = requests.get("https://charts.bitcoin.com/")
+page = requests.get("http://blockchained.com/")
 tree = html.fromstring(page.content)
 
 # store data from webpage in vars
-btc_price = tree.xpath("//p[@id='bb-price']/text()")
+btc_price_usd = tree.xpath("//*[@id='container']/font[1]/b/text()")
+btc_price_eur = tree.xpath("//*[@id='container']/font[2]/b/text()")
 
-print(btc_price)
+print(btc_price_usd)
+print(btc_price_eur)
