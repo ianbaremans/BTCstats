14351e9cdbae629e4dff07d19407e84dfcce6b30
ianbaremans <32411363+ianbaremans@users.noreply.github.com>
2017-10-09 16:58:58 +0200

    Added BTC market cap and coins generated

--- main.py
+++ main.py
@@ -2,14 +2,27 @@
 from lxml import html
 import requests 
 import asciimatics
+import re
 
 # getting the webpage with data and parse it to tree
 page = requests.get("http://blockchained.com/")
 tree = html.fromstring(page.content)
+page.text
+
+# regex for btc generated and market cap
+match_generated = re.search(r"(Coins\ generated\: )(.*)", page.text)
+match_market_cap = re.search(r"(Market\ cap\: )(.*)", page.text)
 
 # store data from webpage in vars
 btc_price_usd = tree.xpath("//*[@id='container']/font[1]/b/text()")
 btc_price_eur = tree.xpath("//*[@id='container']/font[2]/b/text()")
+btc_generated = match_generated.group(2)
+btc_generated = btc_generated[:-5]
+btc_market_cap = match_market_cap.group(2)
+btc_market_cap = btc_market_cap[:-5]
 
 print(btc_price_usd)
 print(btc_price_eur)
+print(btc_generated)
+print(btc_market_cap)
+
