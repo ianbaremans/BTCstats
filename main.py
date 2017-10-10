8485a4798949abb0b35ec447e133c9fe44743b82
ianbaremans <32411363+ianbaremans@users.noreply.github.com>
2017-10-10 12:41:31 +0200

    Added header to the CLI

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # importing the necessary modules
+from __future__ import division
 from lxml import html
 import requests 
 import asciimatics
 import re
+from asciimatics.screen import Screen
+from time import sleep
+from asciimatics.effects import Print
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
 
 # getting the webpage with data and parse it to tree
 page = requests.get("http://blockchained.com/")
@@ -19,13 +25,19 @@ btc_price_eur = tree.xpath("//*[@id='container']/font[2]/b/text()")
 btc_generated = match_generated.group(2)
 btc_market_cap = match_market_cap.group(2)
 
-# slicing the strings to remove html tags
+# slicing the strings to remove unnecessary tags
+btc_price_usd = str(btc_price_usd)
+btc_price_eur = str(btc_price_eur)
+btc_price_usd = btc_price_usd[2:-2]
+btc_price_eur = btc_price_eur[2:-2]
 btc_market_cap = btc_market_cap[:-5]
 btc_generated = btc_generated[:-5]
 
-# printing the data for testing
-print(btc_price_usd)
-print(btc_price_eur)
-print(btc_generated)
-print(btc_market_cap)
+def gui(screen):
+    effects = [
+        Print(screen,FigletText("BITCOIN", font="big"), screen.height // 3 - 8),
+    ]
+    screen.play([Scene(effects, 0)])
+
+Screen.wrapper(gui)
 
