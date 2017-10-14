a74dd9e0377806baf83178ad47d3ad66c01cdc33
ianbaremans <32411363+ianbaremans@users.noreply.github.com>
2017-10-14 13:23:38 +0200

    Code refactor

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import re
 from time import sleep
 import curses
 
+# var for centering everything
+centervar = 63
+
 # ascii art title
-title = "  ____ _____ _______ _____ ____ _____ _   _ ".center(63)+"\n"
-title += " |  _ \_   _|__   __/ ____/ __ \_   _| \ | |".center(63)+"\n"
-title += " | |_) || |    | | | |   | |  | || | |  \| |".center(63)+"\n"
-title += " |  _ < | |    | | | |   | |  | || | | . ` |".center(63)+"\n"
-title += " | |_) || |_   | | | |___| |__| || |_| |\  |".center(63)+"\n"
-title += " |____/_____|  |_|  \_____\____/_____|_| \_|".center(63)+"\n"
+title = "  ____ _____ _______ _____ ____ _____ _   _ ".center(centervar)+"\n"
+title += " |  _ \_   _|__   __/ ____/ __ \_   _| \ | |".center(centervar)+"\n"
+title += " | |_) || |    | | | |   | |  | || | |  \| |".center(centervar)+"\n"
+title += " |  _ < | |    | | | |   | |  | || | | . ` |".center(centervar)+"\n"
+title += " | |_) || |_   | | | |___| |__| || |_| |\  |".center(centervar)+"\n"
+title += " |____/_____|  |_|  \_____\____/_____|_| \_|".center(centervar)+"\n"
 
 # getting the webpage with data and parse it to tree
 page = requests.get("http://blockchained.com/")
@@ -36,12 +39,12 @@ btc_price_eur = btc_price_eur[2:-2].center(11)
 btc_market_cap = btc_market_cap[:-9].center(23)
 btc_generated = btc_generated[:-5].center(13)
 
-data = " "+"="*63+"\n"
+data = " "+"="*centervar+"\n"
 data += " |    USD    |    EUR    |   MARKET CAP in USD   |    COINS    |\n "
-data += "="*63+"\n"
+data += "="*centervar+"\n"
 data += " |{}|{}|{}|{}|\n ".format(
     btc_price_usd, btc_price_eur, btc_market_cap, btc_generated)
-data += "="*63+"\n"
+data += "="*centervar+"\n"
 window = curses.initscr()
 window.clear()
 window.addstr(title)
